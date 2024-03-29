d00ea7211a2dbc0981e76470dab217e155fa805b
Avinash Nagaiah <149421985+NagaiahAvinash@users.noreply.github.com>
2024-03-29 02:44:41 +0000

    Decorator Design Pattern complete

---
+++ Nagaiah/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var component = new ConcreteComponent();
+        Console.WriteLine(component.GetText());
+
+        var plainDecorator = new PlainDecorator(component);
+        Console.WriteLine(plainDecorator.GetText());
+
+        var upperCaseDecorator = new UpperCaseDecorator(component);
+        Console.WriteLine(upperCaseDecorator.GetText());
+
+        var colorDecorator = new ColorDecorator(component);
+        Console.WriteLine(colorDecorator.GetText());
+    }
+}
